00650b497bf4fe3a74cc07151f374b4de8aec9f8
Aylen Cruz <lacuriosa_jael_29@hotmail.com>
2022-01-14 18:51:49 -0300

    primer avance

---
+++ java.js
@@ -0,0 +1,3 @@
+const vendedoras = ["Ada", "Grace", "Hedy", "Sheryl"],
+
+/*const precios = [["Monitor GPRS 3000",200], ["Motherboard ASUS 1500",120 ], ["Monitor ASC 543", 250 ], ["Motherboard ASUS 1200", 100 ], ["Motherboard MZI", 30 ], ["HDD Toyiva", 90 ], ["HDD Wezter Dishital", 75 ], ["RAM Quinston", 110 ], ["RAM Quinston Fury", 230 ]]*/
